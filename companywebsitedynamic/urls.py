16eaa705d331a9bd76e5660eacad6f2446037770
Dineshkumar021617 <dk021617@gmail.com>
2021-01-23 07:57:15 +0000

    mywebsite

--- companywebsitedynamic/urls.py
+++ companywebsitedynamic/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('home',views.home,name='home'),
     path("products",views.products,name='products'),
     path('people',views.people,name='people'),
     path('contactus',views.contactus,name='contactus')
